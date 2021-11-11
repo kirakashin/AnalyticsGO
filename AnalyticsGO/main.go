6e71a6490d59266b2abd654c260191de040db591
kirakashin <kirakashin@mail.ru>
2021-11-11 17:27:16 +0300

    peaks + ip

--- AnalyticsGO/main.go
+++ AnalyticsGO/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -59,22 +60,58 @@ func statHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Count{Count: len(DATA)})
 }
 
+// func collectHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("collecting data")
+// 	w.Header().Set("Content-Type", "application/json")
+// 	var data Data
+// 	_ = json.NewDecoder(r.Body).Decode(&data)
+// 	DATA = append(DATA, data)
+// 	json.NewEncoder(w).Encode(DATA)
+// }
+
 func collectHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("collecting...")
+	fmt.Println("collecting data")
 	w.Header().Set("Content-Type", "application/json")
-	var data Data
+	var data []Data
 	_ = json.NewDecoder(r.Body).Decode(&data)
-	DATA = append(DATA, data)
-	json.NewEncoder(w).Encode(DATA)
+	DATA = append(DATA, data...)
+	// fmt.Println(DATA)
+	// json.NewEncoder(w).Encode(DATA)
 }
 
 func reportOSHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote("OS.xlsx"))
 	w.Header().Set("Content-Type", "application/octet-stream")
 	f := excelize.NewFile()
+	f.NewSheet("All stat")
+	f.DeleteSheet("Sheet1")
+	statPlatform := make(map[string]int)
+	statBrowser := make(map[string]int)
+	f.SetCellValue("All stat", "A1", "Platform")
+	f.SetCellValue("All stat", "B1", "Browser")
 	for i, v := range DATA {
-		f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+1), v.BrowserClientInfo.Platform)
-		f.SetCellValue("Sheet1", "B"+strconv.Itoa(i+1), v.BrowserClientInfo.BrowserClient)
+		f.SetCellValue("All stat", "A"+strconv.Itoa(i+2), v.BrowserClientInfo.Platform)
+		statPlatform[v.BrowserClientInfo.Platform]++
+		f.SetCellValue("All stat", "B"+strconv.Itoa(i+2), v.BrowserClientInfo.BrowserClient)
+		statBrowser[v.BrowserClientInfo.BrowserClient]++
+	}
+	f.NewSheet("Stat Platform")
+	f.SetCellValue("Stat Platform", "A1", "Platform")
+	f.SetCellValue("Stat Platform", "B1", "Count")
+	counter := 1
+	for i, v := range statPlatform {
+		f.SetCellValue("Stat Platform", "A"+strconv.Itoa(counter), i)
+		f.SetCellValue("Stat Platform", "B"+strconv.Itoa(counter), v)
+		counter++
+	}
+	f.NewSheet("Stat Browser")
+	f.SetCellValue("Stat Browser", "A1", "Browser")
+	f.SetCellValue("Stat Browser", "B1", "Count")
+	counter = 1
+	for i, v := range statBrowser {
+		f.SetCellValue("Stat Browser", "A"+strconv.Itoa(counter), i)
+		f.SetCellValue("Stat Browser", "B"+strconv.Itoa(counter), v)
+		counter++
 	}
 	if err := f.SaveAs("OS.xlsx"); err != nil {
 		fmt.Println(err)
@@ -98,7 +135,7 @@ func reportResHandler(w http.ResponseWriter, r *http.Request) {
 type IP struct {
 	Status  string
 	Country string
-	Org     string
+	ISP     string
 }
 
 func reportCountryHandler(w http.ResponseWriter, r *http.Request) {
@@ -107,7 +144,7 @@ func reportCountryHandler(w http.ResponseWriter, r *http.Request) {
 	f := excelize.NewFile()
 	var t IP
 	for i := range DATA {
-		res, _ := http.Get("http://ip-api.com/json/" + DATA[i].BrowserClientInfo.UserIP + "?fields=17409")
+		res, _ := http.Get("http://ip-api.com/json/" + DATA[i].BrowserClientInfo.UserIP + "?fields=16897")
 		ip, _ := ioutil.ReadAll(res.Body)
 		err := json.Unmarshal(ip, &t)
 		if err != nil {
@@ -126,19 +163,76 @@ func reportProviderHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	f := excelize.NewFile()
 	for i := range DATA {
-		res, _ := http.Get("http://ip-api.com/json/" + DATA[i].BrowserClientInfo.UserIP + "?fields=17409")
+		res, _ := http.Get("http://ip-api.com/json/" + DATA[i].BrowserClientInfo.UserIP + "?fields=16897")
 		var t IP
 		ip, _ := ioutil.ReadAll(res.Body)
 		err := json.Unmarshal(ip, &t)
 		if err != nil {
 			fmt.Println(err)
 		}
-		f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+1), t.Org)
+		f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+1), t.ISP)
 	}
 	if err := f.SaveAs("Provider.xlsx"); err != nil {
 		fmt.Println(err)
 	}
 	http.ServeFile(w, r, "Provider.xlsx")
+}
+
+func ipToCP(ipip []string, c chan []IP) {
+	var ips []IP
+
+	x, _ := json.Marshal(ipip)
+	re := bytes.NewReader(x)
+	res, _ := http.Post("http://ip-api.com/batch?fields=16897", "application/json", re)
+	ip, _ := ioutil.ReadAll(res.Body)
+	err := json.Unmarshal(ip, &ips)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c <- ips
+}
+
+func reportCPHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote("CP.xlsx"))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	f := excelize.NewFile()
+	f.NewSheet("All stat")
+	f.DeleteSheet("Sheet1")
+	var ips []IP
+	var ipip []string
+	for _, v := range DATA {
+		ipip = append(ipip, v.BrowserClientInfo.UserIP)
+	}
+	ALERT := 1
+	index := 0
+	c := make(chan []IP)
+	for ALERT <= 15 {
+		if index+100 >= len(ipip) {
+			go ipToCP(ipip[index:], c)
+			break
+		} else {
+			go ipToCP(ipip[index:index+100], c)
+		}
+		index += 100
+		ALERT++
+	}
+	if ALERT == 16 {
+		ALERT--
+	}
+	for ALERT > 0 {
+		ips = append(ips, <-c...)
+		ALERT--
+	}
+
+	for i, v := range ips {
+		f.SetCellValue("All stat", "A"+strconv.Itoa(i+1), v.Country)
+		f.SetCellValue("All stat", "B"+strconv.Itoa(i+1), v.ISP)
+	}
+	if err := f.SaveAs("CP.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+	http.ServeFile(w, r, "CP.xlsx")
 }
 
 type stamp struct {
@@ -167,8 +261,10 @@ func reportPeaksHandler(w http.ResponseWriter, r *http.Request) {
 	f := excelize.NewFile()
 	var timeStamps stampSlice
 	for _, v := range DATA {
-		timeStamps = append(timeStamps, stamp{v.JoinTime[11:19], true, 0})
-		timeStamps = append(timeStamps, stamp{v.LeaveTime[11:19], false, 0})
+		if v.JoinTime[:5] != "0001-" && v.LeaveTime[:5] != "0001-" {
+			timeStamps = append(timeStamps, stamp{v.JoinTime[:19], true, 0})
+			timeStamps = append(timeStamps, stamp{v.LeaveTime[:19], false, 0})
+		}
 	}
 	// fmt.Println(timeStamps)
 	sort.Sort(timeStamps)
@@ -203,6 +299,10 @@ func reportPeaksHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "Peaks.xlsx")
 }
 
+// func reportAllHandler(w http.ResponseWriter, r *http.Request) {
+
+// }
+
 type Port struct {
 	Port string
 }
@@ -216,7 +316,10 @@ func main() {
 	r.HandleFunc("/report_res", reportResHandler).Methods("GET")
 	r.HandleFunc("/report_country", reportCountryHandler).Methods("GET")
 	r.HandleFunc("/report_provider", reportProviderHandler).Methods("GET")
+	r.HandleFunc("/report_cp", reportCPHandler).Methods("GET")
+
 	r.HandleFunc("/report_peaks", reportPeaksHandler).Methods("GET")
+	// r.HandleFunc("/report_all", reportAllHandler).Methods("GET")
 
 	jsonFile, err := os.Open("config.json")
 	if err != nil {
